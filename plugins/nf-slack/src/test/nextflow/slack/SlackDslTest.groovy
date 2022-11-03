267f44e34ab6e6b28368172ae538b3546930be18
Jorge Aguilera <jorge.aguilera@seqera.io>
2022-11-03 11:43:37 +0100

    add DSL test
    fix bug in pipelineTemplate.json when pipeline is not completed yet

---
+++ plugins/nf-slack/src/test/nextflow/slack/SlackDslTest.groovy
@@ -0,0 +1,85 @@
+package nextflow.slack
+
+import nextflow.plugin.Plugins
+import nextflow.plugin.TestPluginDescriptorFinder
+import nextflow.plugin.TestPluginManager
+import nextflow.plugin.extension.PluginExtensionProvider
+import org.pf4j.PluginDescriptorFinder
+import spock.lang.Requires
+import spock.lang.Shared
+import spock.lang.Timeout
+import test.Dsl2Spec
+import test.MockScriptRunner
+
+import java.nio.file.Path
+
+
+/**
+ * @author : jorge <jorge.aguilera@seqera.io>
+ *
+ */
+@Timeout(10)
+class SlackDslTest  extends Dsl2Spec{
+
+    @Shared String pluginsMode
+
+    def setup() {
+        // reset previous instances
+        PluginExtensionProvider.reset()
+        // this need to be set *before* the plugin manager class is created
+        pluginsMode = System.getProperty('pf4j.mode')
+        System.setProperty('pf4j.mode', 'dev')
+        // the plugin root should
+        def root = Path.of('.').toAbsolutePath().normalize()
+        def manager = new TestPluginManager(root){
+            @Override
+            protected PluginDescriptorFinder createPluginDescriptorFinder() {
+                return new TestPluginDescriptorFinder(){
+                    @Override
+                    protected Path getManifestPath(Path pluginPath) {
+                        return pluginPath.resolve('build/resources/main/META-INF/MANIFEST.MF')
+                    }
+                }
+            }
+        }
+        Plugins.init(root, 'dev', manager)
+    }
+
+    def cleanup() {
+        Plugins.stop()
+        PluginExtensionProvider.reset()
+        pluginsMode ? System.setProperty('pf4j.mode',pluginsMode) : System.clearProperty('pf4j.mode')
+    }
+
+    @Requires({System.getenv('SLACK_INCOMING_URL')})
+    def 'should send a notification to slack' () {
+        when:
+        def SCRIPT = '''
+            //hack until https://github.com/nextflow-io/nextflow/issues/3348 is resolved
+            import nextflow.plugin.Plugins
+            import nextflow.slack.SlackExtension
+            //-------
+
+            include {notifySlack} from 'plugin/nf-slack'
+
+            //hack until https://github.com/nextflow-io/nextflow/issues/3348 is resolved
+            def extension = Plugins.getExtension(SlackExtension)
+            extension.init(session)
+            //---
+
+            workflow{
+            }
+            '''
+        and:
+        def config = [
+            slack:[
+                on_start : true,
+                notify_url : System.getenv('SLACK_INCOMING_URL')
+            ]
+        ]
+        def result = new MockScriptRunner(config).setScript(SCRIPT).execute()
+
+        then:
+        noExceptionThrown()
+    }
+}
